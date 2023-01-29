d465e89e4bcbefd112724ad9a107c3382aaedccd
AguJP <agujpa@gmail.com>
2023-01-29 02:27:06 -0800

    Write handlers

--- write_handlers.c
+++ write_handlers.c
@@ -93,8 +93,7 @@ int flags, int width, int prec, int length, char padd, char extra_c)
 {
 	int i, padd_start = 1;
 
-	if (prec == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0'
-	    && width == 0)
+	if (prec == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0' && width == 0)
 		return (0);/* printf(".0d", 0)  no char is printed */
 	if (prec == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0')
 		buffer[ind] = padd = ' ';/* width is displayed with padding ' ' */
@@ -113,19 +112,21 @@ int flags, int width, int prec, int length, char padd, char extra_c)
 		{
 			if (extra_c)
 				buffer[--ind] = extra_c;
-			return (write(1, &buffer[ind], length) +
-				write(1, &buffer[1], i - 1));
+			return (write(1, &buffer[ind], length) + write(1, &buffer[1], i - 1));
 		}
 		else if (!(flags & F_MINUS) && padd == ' ')/* extra char to left of buff */
+		{
 			if (extra_c)
 				buffer[--ind] = extra_c;
-			return (write(1, &buffer[1], i - 1) +
-				write(1, &buffer[ind], length));
+			return (write(1, &buffer[1], i - 1) + write(1, &buffer[ind], length));
+		}
 		else if (!(flags & F_MINUS) && padd == '0')/* extra char to left of padd */
+		{
 			if (extra_c)
 				buffer[--padd_start] = extra_c;
 			return (write(1, &buffer[padd_start], i - padd_start) +
 			write(1, &buffer[ind], length - (1 - padd_start)));
+		}
 	}
 	if (extra_c)
 		buffer[--ind] = extra_c;
